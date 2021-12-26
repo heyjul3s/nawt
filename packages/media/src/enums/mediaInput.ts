aae15d2ae55e1ce3f59ff1e76b5cfbf15f10ae87
heyjul3s <julian.cy.low@gmail.com>
2021-12-26 11:59:41 +1100

    fix pointer and any-pointer constants

--- packages/media/src/enums/mediaInput.ts
+++ packages/media/src/enums/mediaInput.ts
@@ -3,26 +3,31 @@ export enum MediaInputQueryKeys {
   'anyHoverNone' = 'anyHoverNone',
   'hover' = 'hover',
   'hoverNone' = 'hoverNone',
-  'anyPointer' = 'anyPointer',
+  'anyPointerCoarse' = 'anyPointerCoarse',
+  'anyPointerFine' = 'anyPointerFine',
   'anyPointerNone' = 'anyPointerNone',
-  'pointer' = 'pointer',
+  'pointerCoarse' = 'pointerCoarse',
+  'pointerFine' = 'pointerFine',
   'pointerNone' = 'pointerNone'
 }
 
 export type MediaInputQueries = {
   [MediaInputQueryKeys.anyHover]: AnyHover;
   [MediaInputQueryKeys.anyHoverNone]: AnyHoverNone;
-  [MediaInputQueryKeys.anyPointer]: AnyPointer;
+  [MediaInputQueryKeys.anyPointerCoarse]: AnyPointerCoarse;
+  [MediaInputQueryKeys.anyPointerFine]: AnyPointerFine;
   [MediaInputQueryKeys.anyPointerNone]: AnyPointerNone;
   [MediaInputQueryKeys.hover]: Hover;
   [MediaInputQueryKeys.hoverNone]: HoverNone;
-  [MediaInputQueryKeys.pointer]: Pointer;
   [MediaInputQueryKeys.pointerNone]: PointerNone;
+  [MediaInputQueryKeys.pointerCoarse]: PointerCoarse;
+  [MediaInputQueryKeys.pointerFine]: PointerFine;
 };
 
 export type AnyHover = 'any-hover: hover';
 export type AnyHoverNone = 'any-hover: none';
-export type AnyPointer = 'any-pointer: pointer';
+export type AnyPointerCoarse = 'any-pointer: coarse';
+export type AnyPointerFine = 'any-pointer: fine';
 export type AnyPointerNone = 'any-pointer: none';
 export type DisplayModeBrowser = 'display-mode: browser';
 export type DisplayModeFullScreen = 'display-mode: fullscreen';
@@ -32,16 +37,19 @@ export type Hover = 'hover: hover';
 export type HoverNone = 'hover: none';
 export type OrientationLandscape = 'orientation: landscape';
 export type OrientationPortrait = 'orientation: portrait';
-export type Pointer = 'pointer: pointer';
 export type PointerNone = 'pointer: none';
+export type PointerCoarse = 'pointer: coarse';
+export type PointerFine = 'pointer: fine';
 
 export const mediaInputQueries: MediaInputQueries = {
   [MediaInputQueryKeys.anyHover]: 'any-hover: hover',
   [MediaInputQueryKeys.anyHoverNone]: 'any-hover: none',
   [MediaInputQueryKeys.hover]: 'hover: hover',
   [MediaInputQueryKeys.hoverNone]: 'hover: none',
-  [MediaInputQueryKeys.anyPointer]: 'any-pointer: pointer',
+  [MediaInputQueryKeys.anyPointerCoarse]: 'any-pointer: coarse',
+  [MediaInputQueryKeys.anyPointerFine]: 'any-pointer: fine',
   [MediaInputQueryKeys.anyPointerNone]: 'any-pointer: none',
-  [MediaInputQueryKeys.pointer]: 'pointer: pointer',
-  [MediaInputQueryKeys.pointerNone]: 'pointer: none'
+  [MediaInputQueryKeys.pointerNone]: 'pointer: none',
+  [MediaInputQueryKeys.pointerCoarse]: 'pointer: coarse',
+  [MediaInputQueryKeys.pointerFine]: 'pointer: fine'
 };
