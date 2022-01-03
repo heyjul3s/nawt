449726bc77f226fe2bf5d6358cae5c2aafe2d96e
heyjul3s <julian.cy.low@gmail.com>
2022-01-03 13:47:47 +1100

    Refactor Units - refactor and reorganise units lib - add bundler to package - add units.test

--- packages/units/src/index.ts
+++ packages/units/src/index.ts
@@ -1,2 +1,13 @@
-export { pxToEm } from './px-to-em';
-export { pxToRem } from './px-to-rem';
+import { convertValue } from './convert-value';
+
+export const pxToEm = (value: string, baseSize: number = 16) =>
+  convertValue(value, 'em', baseSize);
+
+export const pxToRem = (value: string, baseSize: number = 16) =>
+  convertValue(value, 'rem', baseSize);
+
+export const emToPx = (value: string, baseSize: number = 16) =>
+  convertValue(value, 'px', baseSize);
+
+export const remToPx = (value: string, baseSize: number = 16) =>
+  convertValue(value, 'px', baseSize);
