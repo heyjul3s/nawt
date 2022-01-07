466de0e1cab64e7504de786c6b1ceabf373e4172
heyjul3s <julian.cy.low@gmail.com>
2022-01-07 18:24:47 +1100

    update TStyledObject Props typing

--- packages/styled/src/typings.ts
+++ packages/styled/src/typings.ts
@@ -2,9 +2,23 @@ import * as CSS from 'csstype';
 import type { StyledComponent } from 'styled-components';
 import type { Scale, Config, VariantArgs, styleFn } from 'styled-system';
 
+import type {
+   BackgroundProps,
+      BorderProps,
+      ColorProps,
+      DisplayProps,
+      LayoutProps,
+      PositionProps,
+      ShadowProps,
+      SpaceProps,
+      TypographyProps,
+} from 'styled-system'
+
 export type TCSSObjectWithScale = CSS.Properties<string | number | Scale>;
 export type TCSSPseudos = { [K in CSS.Pseudos]?: TCSSObjectWithScale };
 export type TScalableCSSObject = TCSSObjectWithScale & TCSSPseudos;
+
+export type BaseProps = BackgroundProps & BorderProps & ColorProps & DisplayProps & LayoutProps & PositionProps & ShadowProps & SpaceProps & TypographyProps;
 
 export type TStyledObjectProps = {
   attrs: any;
@@ -13,7 +27,7 @@ export type TStyledObjectProps = {
   system: Config;
 };
 
-export type TStyledObject<Props = void, ThemeType = void> = StyledComponent<
+export type TStyledObject<Props extends BaseProps, ThemeType = void> = StyledComponent<
   keyof JSX.IntrinsicElements,
   ThemeType | any,
   Props | any,
